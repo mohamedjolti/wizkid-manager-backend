8db82e3e15176b353c653ce37fe347066ba526a8
mohamed jolti <mohamedelkholti062@gmail.com>
2022-10-08 19:44:28 +0200

    fix update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -83,7 +83,6 @@ class UserController extends Controller
         $rules = [
             'name' => 'required',
             'role' => 'required|in:boss,developer,designer,intern',
-            'email' => 'email|required',
             'picture' => "required",
             'phone_number' => 'regex:/[0-9]{9}/'
 
@@ -93,11 +92,9 @@ class UserController extends Controller
             'name.required' => 'name is required',
             'role.required' => "role is required",
             'role.in' => "role should be boss,developer,designer  or intern",
-            'email.required' => "email is required",
             'email.email'    => "the format of the email should be valid",
             'picture.file' => "the picture should be a file",
             'picture.image' => "the picture should be file of type image",
-            'picture.required' => "the picture is required",
             'phone_number.regex' => "phone format not valid"
 
         ];
@@ -112,15 +109,17 @@ class UserController extends Controller
                 $user = User::find($id);
                 $user->name = $request->name;
                 $user->role = $request->role;
-                $user->email = $request->email;
-                $user->phone_number = $request->phone_number;
-                //generate password
-                $password = Str::random(8);
-                $user->password = Hash::make($password);
+                if ($request->email)
+                    $user->email = $request->email;
 
-                //update picture
-                $user->picture = date("Y_m_d_His") . '_' . $request->file("picture")->getClientOriginalName();
-                FileService::Upload($request->file("picture"));
+                if ($request->phone_number)
+                    $user->phone_number = $request->phone_number;
+
+                if ($request->file("picture")) {
+                    //update picture
+                    $user->picture = date("Y_m_d_His") . '_' . $request->file("picture")->getClientOriginalName();
+                    FileService::Upload($request->file("picture"));
+                }
                 $user->save();
                 return response()->json($user, 201);
             } catch (Exception $e) {
@@ -133,7 +132,7 @@ class UserController extends Controller
 
     public function getUsers()
     {
-        $users = User::select("id","name", "role", "picture")->get();
+        $users = User::select("id", "name", "role", "picture")->get();
         return response()->json($users->all(), 201);
     }
 
@@ -149,47 +148,47 @@ class UserController extends Controller
         }
     }
 
-    public function getUsersCompleteInformations(){
+    public function getUsersCompleteInformations()
+    {
         $users = User::all();
         return response()->json($users->all(), 201);
     }
-    
-    public function fire($id){
-        if(auth()->user()->id == $id){
+
+    public function fire($id)
+    {
+        if (auth()->user()->id == $id) {
             return response()->json("Sorry you can't fire yourself", 401);
         }
-        $user=User::find($id);
-        if(!$user){
+        $user = User::find($id);
+        if (!$user) {
             return response()->json("Wizkid not found", 401);
         }
-        if($user->is_fired == 0){
-            $user->is_fired=1;
+        if ($user->is_fired == 0) {
+            $user->is_fired = 1;
             $user->save();
             Mail::to($user->email)->queue(new FireMail());
             return response()->json("Wizkid is fired", 200);
-        }else{
+        } else {
             return response()->json("Wizkid is alredey fired", 401);
-
         }
     }
 
-    public function unfire($id){
-        if(auth()->user()->id == $id){
+    public function unfire($id)
+    {
+        if (auth()->user()->id == $id) {
             return response()->json("Sorry you can't unfire yourself", 401);
         }
-        $user=User::find($id);
-        if(!$user){
+        $user = User::find($id);
+        if (!$user) {
             return response()->json("Wizkid not found ", 401);
         }
-        if($user->is_fired == 1){
-            $user->is_fired=0;
+        if ($user->is_fired == 1) {
+            $user->is_fired = 0;
             $user->save();
             Mail::to($user->email)->queue(new unFireMail());
             return response()->json("Wizkid is back", 200);
-        }else{
+        } else {
             return response()->json("Wizkid is not fired yet !", 401);
-
         }
     }
-  
 }
